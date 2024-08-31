4b52db363f417939fbc1d39141c1f491e6f88ddb
Tanu Bhardwaj <tanu.b@drinkprime.in>
2024-08-31 22:28:38 +0530

    -Registration UI

---
+++ app/src/main/java/com/app/sudhar/registration/RegistrationFragment.kt
@@ -0,0 +1,22 @@
+package com.app.sudhar.registration
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.app.sudhar.databinding.FragmentRegistrationBinding
+
+
+class RegistrationFragment : Fragment() {
+    private lateinit var binding: FragmentRegistrationBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentRegistrationBinding.inflate(inflater)
+        return binding.root
+    }
+}
