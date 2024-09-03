9e917e8271766384e358706ad98ec40e7bdef8a2
Tanu Bhardwaj <tanu.b@drinkprime.in>
2024-09-03 23:00:33 +0530

    -Calculator UI

--- app/src/main/java/com/app/sudhar/home/SurfaceMiningCalculatorFragment.kt
+++ app/src/main/java/com/app/sudhar/home/SurfaceMiningCalculatorFragment.kt
@@ -10,6 +10,7 @@ import com.app.sudhar.databinding.FragmentSurfaceMiningCalculatorBinding
 
 class SurfaceMiningCalculatorFragment : Fragment() {
     private lateinit var binding:FragmentSurfaceMiningCalculatorBinding
+    private var step = 1
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,5 +18,73 @@ class SurfaceMiningCalculatorFragment : Fragment() {
         // Inflate the layout for this fragment
         binding  = FragmentSurfaceMiningCalculatorBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+    private fun init(){
+        initClickListener()
+        initSetViews()
+    }
+
+    private fun initSetViews() {
+
+    }
+
+    private fun initClickListener()=binding.apply{
+        nextBtn.setOnClickListener {
+            when(step){
+                1->{
+                    tvStepNum.text = "Step 2->Extraction"
+                    tvMachineName1.text = "Front-End Loader"
+                    tvMachineName2.text = "Truck (Haul Truck)"
+                    tvMachineName3.text = "Continuous Miner"
+                    tvMachineName4.text = "Electric Rope Shovel"
+                }
+                2->{
+                    tvStepNum.text = "Step 3->Transportation"
+                    tvMachineName1.text = "Load-Haul-Dump(LHD)"
+                    tvMachineName2.text = "Truck (Haul Truck)"
+                    tvMachineName3.text = "Shuttle Car"
+                    tvMachineName4.text = "Conveyor System"
+                }
+                3->{
+                    tvStepNum.text = "Step 4->Processing and Handling"
+                    tvMachineName1.visibility = View.GONE
+                    tvMachineName2.visibility = View.GONE
+                    tvMachinesDiesel.visibility = View.GONE
+                    etMachine1.visibility = View.GONE
+                    etMachine2.visibility = View.GONE
+                    tvMachineName3.text = "Crusher"
+                    tvMachineName4.text = "Screening Plant"
+                    tvMachinesElectricity.visibility = View.VISIBLE
+                    tvMachineName3.visibility = View.VISIBLE
+                    tvMachineName4.visibility = View.VISIBLE
+                    etMachine3.visibility = View.VISIBLE
+                    etMachine4.visibility = View.VISIBLE
+                }
+                4->{
+                    tvMachineName1.visibility = View.VISIBLE
+                    tvMachineName2.visibility = View.VISIBLE
+                    tvMachinesDiesel.visibility = View.VISIBLE
+                    etMachine1.visibility = View.VISIBLE
+                    etMachine2.visibility = View.VISIBLE
+                    tvStepNum.text = "Step 5->Site Reclamation"
+                    nextBtn.text = "Calculate"
+                    tvMachineName1.text = "Bulldozer"
+                    tvMachineName2.text = "Excavator"
+                    tvMachinesElectricity.visibility = View.GONE
+                    tvMachineName3.visibility = View.GONE
+                    tvMachineName4.visibility = View.GONE
+                    etMachine3.visibility = View.GONE
+                    etMachine4.visibility = View.GONE
+                }
+                5->{
+
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/app/sudhar/home/UndergroundMiningCalculatorFragment.kt
+++ app/src/main/java/com/app/sudhar/home/UndergroundMiningCalculatorFragment.kt
@@ -10,6 +10,7 @@ import com.app.sudhar.databinding.FragmentUndergroundMiningCalculatorBinding
 
 class UndergroundMiningCalculatorFragment : Fragment() {
     private lateinit var binding: FragmentUndergroundMiningCalculatorBinding
+    private var step = 1
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,5 +18,73 @@ class UndergroundMiningCalculatorFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentUndergroundMiningCalculatorBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+    private fun init(){
+        initClickListener()
+        initSetViews()
+    }
+
+    private fun initSetViews() {
+
+    }
+
+    private fun initClickListener()=binding.apply{
+        nextBtn.setOnClickListener {
+            when(step){
+                1->{
+                    tvStepNum.text = "Step 2->Extraction"
+                    tvMachineName1.text = "Front-End Loader"
+                    tvMachineName2.text = "Truck (Haul Truck)"
+                    tvMachineName3.text = "Continuous Miner"
+                    tvMachineName4.text = "Shuttle Car"
+                }
+                2->{
+                    tvStepNum.text = "Step 3->Transportation"
+                    tvMachineName1.text = "Load-Haul-Dump(LHD)"
+                    tvMachineName2.text = "Truck (Haul Truck)"
+                    tvMachineName3.text = "Shuttle Car"
+                    tvMachineName4.text = "Conveyor System"
+                }
+                3->{
+                    tvStepNum.text = "Step 4->Ventilation"
+                    tvMachineName1.visibility = View.GONE
+                    tvMachineName2.visibility = View.GONE
+                    tvMachinesDiesel.visibility = View.GONE
+                    etMachine1.visibility = View.GONE
+                    etMachine2.visibility = View.GONE
+                    tvMachineName3.text = "Crusher"
+                    tvMachineName4.text = "Screening Plant"
+                }
+                4->{
+                    tvStepNum.text = "Step 5->Water Management"
+                    tvMachineName1.text = "Bulldozer"
+                    tvMachineName2.text = "Excavator"
+                    tvMachinesElectricity.visibility = View.GONE
+                    tvMachineName3.visibility = View.GONE
+                    tvMachineName4.visibility = View.GONE
+                    etMachine3.visibility = View.GONE
+                    etMachine4.visibility = View.GONE
+                }
+                5->{
+                    tvStepNum.text = "Step 5->Processing and handling"
+                    nextBtn.text = "Calculate"
+                    tvMachineName1.text = "Bulldozer"
+                    tvMachineName2.text = "Excavator"
+                    tvMachinesElectricity.visibility = View.GONE
+                    tvMachineName3.visibility = View.GONE
+                    tvMachineName4.visibility = View.GONE
+                    etMachine3.visibility = View.GONE
+                    etMachine4.visibility = View.GONE
+                }
+                6->{
+
+                }
+            }
+        }
     }
 }
